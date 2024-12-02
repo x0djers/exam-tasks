4079036937e945d18038ba0530230393695eeebf
Maksim Mikhailov <87376297+CodeAnge1@users.noreply.github.com>
2024-12-02 11:07:15 +0300

    Task 78 solved

---
+++ task78.c
@@ -0,0 +1,104 @@
+/*
+78. Дана последовательность целых чисел, все элементы которой различны.
+    Вывести в порядке убывания те члены последовательности, которые не находятся
+    между минимальным и максимальным.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ELEMENTS_COUNT 1000
+
+int getSeqLen(int *elemCount) {
+    printf("Программа принимает максимум 1000 чисел!\nВведите количество элементов: ");
+    scanf("%d", elemCount);
+    return (0 < *elemCount && *elemCount <= MAX_ELEMENTS_COUNT);
+}
+
+void getSeqInput(int elemCount, int *arrIn) {
+    printf("Введите элементы последовательности (каждое с новой строки):\n");
+    
+    for (int iter = 0; iter < elemCount; iter++) {
+        scanf("%d", &arrIn[iter]);
+    }
+}
+
+int findMinIndexOfArr(int *arr, int size) {
+    int minIndex = 0;
+    for (int iter = 1; iter < size; iter++) {
+        if (arr[iter] < arr[minIndex]) {
+            minIndex = iter;
+        }
+    }
+    return minIndex;
+}
+
+int findMaxIndexOfArr(int *arr, int size) {
+    int maxIndex = 0;
+    for (int iter = 1; iter < size; iter++) {
+        if (arr[iter] > arr[maxIndex]) {
+            maxIndex = iter;
+        }
+    }
+    return maxIndex;
+}
+
+void getSlice(int startIndex, int endIndex, int *arrIn, int **arrOut) {
+    
+    int arrSize = endIndex - startIndex + 1;
+    *arrOut = (int *)malloc(sizeof(int) * arrSize);
+    for (int iter = startIndex; iter <= endIndex; iter++) {
+        (*arrOut)[iter - startIndex] = arrIn[iter];
+    }
+}
+
+void printResult(int *arr, int size) {
+    printf("Упорядоченная часть последовательности: ");
+    for (int iter = 0; iter < size; iter++) {
+        printf("%d ", arr[iter]);
+    }
+    printf("\n");
+}
+
+void swap(int* const a, int* const b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int *arr, int size) {
+    for (int iter=0; iter < size - 1; iter++) {
+        for (int cur = 0; cur < size - iter - 1; cur++) {
+            if (*(arr + cur) > *(arr + cur + 1))
+                swap(arr + cur, arr + cur + 1);
+        }
+    }
+}
+
+int main() {
+
+    int elemCount = 0;
+    if (getSeqLen(&elemCount)) {
+        int inputArray[elemCount];
+
+        getSeqInput(elemCount, inputArray);
+        
+        int minIndex = findMinIndexOfArr(inputArray, elemCount);
+        int maxIndex = findMaxIndexOfArr(inputArray, elemCount);
+        
+        int *resultArray;
+        
+        int arraySize = maxIndex - minIndex + 1;
+        printf("arraySize: %d\n", arraySize);
+
+        getSlice(minIndex, maxIndex, inputArray, &resultArray);
+        bubbleSort(resultArray, arraySize);
+        printResult(resultArray, arraySize);
+
+        free(resultArray);
+
+    } else {
+        printf("Некоректное число элементов последовательности!");
+    }
+    return 0;
+}
